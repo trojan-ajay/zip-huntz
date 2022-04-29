fa785fd6b27c4bf21477151020771b1d0769507f
AJAY <ajayayay4309@gmail.com>
2022-04-29 23:27:36 +0530

    boots

--- src/Components/Default.js
+++ src/Components/Default.js
@@ -1,31 +1,27 @@
-import React from 'react';
-
+import React from "react";
 
 function Default(props) {
-
   return (
     <div>
-        <header className="header">
-				<div className="row">
-					<div className="col">
-							<div className="header_content_inner d-flex flex-row align-items-end justify-content-start">
-								<div className="logo">ZIP-HUNTZ</div>
-								<nav className="main_nav">
-									<ul className="d-flex flex-row align-items-start justify-content-start">
-										<li className="active"><button className='help'>Home</button></li>
-										<li><button className='help'>Help</button></li>
-										<li>Contact us : +91-9876543210</li>
-									</ul>
-								</nav>
-                            </div>
-                    </div>
-                </div>
-            </header>
-        <div className='content'>
-            {props.children}
+    <div class="container-fluid">
+      <div class="row p-2 bg">
+        <div class="col-sm-4 logo" align="left">
+          ZIP-HUNTZ
         </div>
+        <div class="col-sm-2 b" align="left">
+          <button className="button">Home</button>
+        </div>
+        <div class="col-sm-3 b" align="left">
+          <button className="button">Help</button>
+        </div>
+        <div class="col-sm-3 b" align="left">
+          <button className="button">contact-us</button>
+        </div>
+      </div>
     </div>
-  )
+    <div className="content">{props.children}</div>
+    </div>
+  );
 }
 
-export default Default
+export default Default;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,45 +1,63 @@
-import React, { useEffect } from 'react'
-import Default from '../Components/Default'
-import $ from 'jquery'
-import 'jquery-ui-dist/jquery-ui'
+import React, { useEffect } from "react";
+import Default from "../Components/Default";
+import $ from "jquery";
+import "jquery-ui-dist/jquery-ui";
 
 function Home() {
-  useEffect(()=>{
-  $('.form__input').focus(function(){
-    $(this).attr('placeholder',' ');
-    $(".form__label").css('visibility','visible');
-  });
-  $('.form__input').focusout(function(){
-    $(this).attr('placeholder','zip-code');
-    $(".form__label").css('visibility','hidden'); 
-  }); 
-  $(".help").click(()=>{
-    $('#aj').css("visibility","visible")
+  useEffect(() => {
+    $(".form__input").focus(function () {
+      $(this).attr("placeholder", " ");
+      $(".form__label").css("visibility", "visible");
+    });
+    $(".form__input").focusout(function () {
+      $(this).attr("placeholder", "zip-code");
+      $(".form__label").css("visibility", "hidden");
+    });
     $("#aj").dialog({
-      title:"Help",
-      modal:true,
-    })
-  })
-},[])
-  function handler(e){
-    e.preventDefault()
+      title: "Help",
+      modal: true,
+      autoOpen: false,
+    });
+    $(".help").click(() => {
+      $("#aj").dialog("open");
+    });
+  }, []);
+  function handler(e) {
+    e.preventDefault();
   }
   return (
     <Default>
-      <div className='home'>
-          <div className='form' >
-            <div className='bgform'>             
-              <form>
-                <input type="text" className="form__input" id="name" required="" placeholder='zip-code'/>
-                <label id="name" className="form__label tog">zip-code</label>
-                <button className="button-17" type='submit' onClick={handler}>Search</button>
-              </form>
-            </div> 
-          </div> 
+      <div className="container-fluid mt-2 cg " align="center">
+        <div className="row">
+          <div className="col-sm-4 offset-4">
+            <div className="container-fluid home mb-5 mt-5 d-flex justify-content-center align-items-center">
+              <div className="container-fluid p-3 mt-2">
+                <div class="row">
+                  <div className="col-sm hg p-2 mb-1 ">ZIP-HUNTZ</div>
+                </div>
+                <div className="row spacex ">
+                  <div className="col-sm d-flex justify-content-end">- Enter the appropiate pin-code</div>
+                </div>
+                <div class="row">
+                  <div class="col-sm">
+                    <input type="text" placeholder="Enter zip here" />
+                  </div>
+                </div>
+                <div class="row">
+                  <div className="space"></div>
+                </div>
+                <div class="row" align="center">
+                  <div class="col-sm">
+                    <button className="search">click</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div id='aj'>ajay</div>
-   </Default>
-  )
+    </Default>
+  );
 }
 
-export default Home
+export default Home;
